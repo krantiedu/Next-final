db86c93a06dba97f21a80f75651b9e33e7babc20
jpatidar9826 <jpatidar9826@gmail.com>
2023-07-02 00:47:55 +0530

    tutor section done fixed the partner sec

--- component/school_partner.js
+++ component/school_partner.js
@@ -1,36 +1,58 @@
-import styles from '@/styles/achivement.module.css';
-import Image from 'next/image';
+import styles from "@/styles/achivement.module.css";
+import Image from "next/image";
 
 const School = () => {
-    return (
-        <section className={styles.wrapper}>
-            <div className={styles.school_sec}>
-                <h1 className={styles.header2}>School Partners</h1>
-                <div className={styles.school_partners}>
-                    <div className={styles.item1}>
-                        <Image src='/favicon.ico' style={{"margin-bottom":"10px"}} alt="icon" width={90} height={90} />
-                        <p>D.P. School</p>
-                    </div>
-                    <div className={styles.item1}>
-                        <Image src='/favicon.ico' style={{"margin-bottom":"10px"}} alt="icon" width={90} height={90} />
-                        <p>D.P. School</p>
-                    </div>
-                    <div className={styles.item1}>
-                        <Image src='/favicon.ico' style={{"margin-bottom":"10px"}} alt="icon" width={90} height={90} />
-                        <p>D.P. School</p>
-                    </div>
-                    <div className={styles.item1}>
-                        <Image src='/favicon.ico' style={{"margin-bottom":"10px"}} alt="icon" width={90} height={90} />
-                        <p>D.P. School</p>
-                    </div>
-                    <div className={styles.item1}>
-                        <Image src='/favicon.ico' style={{"margin-bottom":"10px"}} alt="icon" width={90} height={90} />
-                        <p>D.P. School</p>
-                    </div>
-                </div>
-            </div>
-        </section>
-    );
-}
+  return (
+    <div className={styles.school_section}>
+      <h1>School Partners</h1>
+      <div className={styles.school_card_wrap}>
+        <div className={styles.school_card}>
+          <div className={styles.school_logo_wrap}>
+            <Image
+              src="https://img.freepik.com/premium-vector/campus-collage-university-education-logo-design-template_7492-61.jpg?w=740"
+              alt="icon"
+              width={200}
+              height={200}
+            />
+          </div>
+          <p>D.P. School</p>
+        </div>
+        <div className={styles.school_card}>
+          <div className={styles.school_logo_wrap}>
+            <Image
+              src="https://img.freepik.com/free-vector/gradient-school-logo-design-template_23-2149664332.jpg?size=626&ext=jpg"
+              alt="icon"
+              width={200}
+              height={200}
+            />
+          </div>
+          <p>D.P. School</p>
+        </div>
+        <div className={styles.school_card}>
+          <div className={styles.school_logo_wrap}>
+            <Image
+              src="https://img.freepik.com/free-vector/gradient-elementary-school-logo-design_23-2149626923.jpg?size=626&ext=jpg"
+              alt="icon"
+              width={200}
+              height={200}
+            />
+          </div>
+          <p>D.P. School</p>
+        </div>
+        <div className={styles.school_card}>
+          <div className={styles.school_logo_wrap}>
+            <Image
+              src="https://img.freepik.com/premium-vector/luxury-university-logo-design_139869-120.jpg?size=626&ext=jpg"
+              alt="icon"
+              width={200}
+              height={200}
+            />
+          </div>
+          <p>D.P. School</p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default School;
--- pages/index.js
+++ pages/index.js
@@ -12,17 +12,18 @@ import Pop_Courses from "@/component/top_courses";
 import Achivement from "@/component/achivement_sec";
 import School from "@/component/school_partner";
 import GoToTop from "@/partials/gototop";
+import TutorCard from "@/component/TutorCard";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   const [isLoggedin, setIsLoggedin] = useState(false);
 
   useEffect(() => {
-    const token = localStorage.getItem('token');
+    const token = localStorage.getItem("token");
     if (!!token) {
-      return setIsLoggedin(true)
+      return setIsLoggedin(true);
     } else {
-      setIsLoggedin(false)
+      setIsLoggedin(false);
     }
   }, []);
 
@@ -31,20 +32,31 @@ export default function Home() {
       {/*Main Section */}
       <div className="main_section">
         <div className="main_content_wrap">
-          <p className="main_content_eyeCatcher"><span>START</span>LEARNING FROM TODAY</p>
+          <p className="main_content_eyeCatcher">
+            <span>START</span>LEARNING FROM TODAY
+          </p>
           <h1>The Best Way For Your Learning</h1>
           <div className="main_content_descript">
             <p>
-            Welcome to KRANTI - an exceptional and empowering realm that paves the way for unparalleled learning, seamlessly blending ease and delight.
+              Welcome to KRANTI - an exceptional and empowering realm that paves
+              the way for unparalleled learning, seamlessly blending ease and
+              delight.
             </p>
           </div>
           <div className="main_content_btn_wrap">
-            <Link href={isLoggedin ? `/dashboard` : `/login`}><div className="get_started_button"><FontAwesomeIcon icon={faArrowRight} />Get Started</div></Link>
-            <div className="demo_button"><FontAwesomeIcon icon={faCirclePlay} />Watch Demo</div>
+            <Link href={isLoggedin ? `/dashboard` : `/login`}>
+              <div className="get_started_button">
+                <FontAwesomeIcon icon={faArrowRight} />
+                Get Started
+              </div>
+            </Link>
+            <div className="demo_button">
+              <FontAwesomeIcon icon={faCirclePlay} />
+              Watch Demo
+            </div>
           </div>
         </div>
       </div>
-      
 
       {/*Review Section */}
       <div className="review_section">
@@ -59,7 +71,9 @@ export default function Home() {
               />
             </div>
             <div className="review_card_rev_wrap">
-              <p className="review_card_rev">There are many variations of passages of Lorem Ipsum available</p>
+              <p className="review_card_rev">
+                There are many variations of passages of Lorem Ipsum available
+              </p>
               <div className="card_footer">
                 <p>78 student</p>
                 <p>⭐⭐⭐⭐⭐</p>
@@ -76,7 +90,9 @@ export default function Home() {
               />
             </div>
             <div className="review_card_rev_wrap">
-              <p className="review_card_rev">There are many variations of passages of Lorem Ipsum available</p>
+              <p className="review_card_rev">
+                There are many variations of passages of Lorem Ipsum available
+              </p>
               <div className="card_footer">
                 <p>78 student</p>
                 <p>⭐⭐⭐⭐⭐</p>
@@ -93,7 +109,9 @@ export default function Home() {
               />
             </div>
             <div className="review_card_rev_wrap">
-              <p className="review_card_rev">There are many variations of passages of Lorem Ipsum available</p>
+              <p className="review_card_rev">
+                There are many variations of passages of Lorem Ipsum available
+              </p>
               <div className="card_footer">
                 <p>78 student</p>
                 <p>⭐⭐⭐⭐⭐</p>
@@ -103,6 +121,39 @@ export default function Home() {
         </div>
       </div>
       <Pop_Courses />
+      {/* Top Tutors */}
+      <div className="tutor_section">
+        <h1 className="section_heading">Our Teachers</h1>
+        <div className="tutor_card_wrap">
+          <TutorCard
+            imgSrc={`https://images.unsplash.com/photo-1569779213435-ba3167dde7cc?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8bWFuJTIwZ2xhc3Nlc3xlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60`}
+            tutName={`John Doe`}
+            tutField={`Software Developer`}
+            tutDet={`t is a long established fact that a reader will be distracted by the
+        readable content of a page when looking at its layout. The point of
+        using Lorem Ipsum is that it has a more-or-less normal distribution of
+        letters, as opposed to using 'Content here, content here',`}
+          />
+          <TutorCard 
+            imgSrc={`https://images.unsplash.com/photo-1590650213165-c1fef80648c4?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80`}
+            tutName={`John Doe`}
+            tutField={`Software Developer`}
+            tutDet={`t is a long established fact that a reader will be distracted by the
+        readable content of a page when looking at its layout. The point of
+        using Lorem Ipsum is that it has a more-or-less normal distribution of
+        letters, as opposed to using 'Content here, content here',`}
+          />
+          <TutorCard 
+            imgSrc={`https://images.unsplash.com/photo-1583468982228-19f19164aee2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fHRlYWNoZXJ8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60`}
+            tutName={`John Doe`}
+            tutField={`Software Developer`}
+            tutDet={`t is a long established fact that a reader will be distracted by the
+        readable content of a page when looking at its layout. The point of
+        using Lorem Ipsum is that it has a more-or-less normal distribution of
+        letters, as opposed to using 'Content here, content here',`}
+          />
+        </div>
+      </div>
       <Achivement />
       <School />
     </div>
