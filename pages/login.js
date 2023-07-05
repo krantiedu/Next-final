05207e92caedbd155fa2d742f0923fcee061f313
krantiedu <128422527+krantiedu@users.noreply.github.com>
2023-07-05 08:05:19 +0530

    Update login.js

--- pages/login.js
+++ pages/login.js
@@ -83,7 +83,7 @@ export default function Login() {
 
         <Formik
           initialValues={initialValues}
-          validationSchema={signInSchema}
+          //validationSchema={signInSchema}
           onSubmit={(values) => {
             submitHandler(values);
           }}
