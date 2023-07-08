8f9fc61a4f813913c09c10c289aa15f84d84dc5c
nikhil-user123 <nikhilprajapat602@gmail.com>
2023-07-08 21:42:34 +0530

    added analytics page

--- helper/constants.js
+++ helper/constants.js
@@ -2,40 +2,81 @@
 
 export const Courses = [
     {
-        id:"0",
+        id: "0",
         url: "/course_img.jpg",
-        subject : "Science",
+        subject: "Science",
         desctiption: "Code has to be executed after the state has been updated Place that code in the call back function which is the second argument to the setState method.",
     },
     {
-        id:"2",
+        id: "2",
         url: "/course_img.jpg",
-        subject : "Maths",
+        subject: "Maths",
         desctiption: "Code has to be executed after the state has been updated Place that code in the call back function which is the second argument to the setState method.",
     },
     {
-        id:"3",
+        id: "3",
         url: "/course_img.jpg",
-        subject : "Bio",
+        subject: "Bio",
         desctiption: "Code has to be executed after the state has been updated Place that code in the call back function which is the second argument to the setState method.",
     },
     {
-        id:"4",
+        id: "4",
         url: "/course_img.jpg",
-        subject : "Chemistry",
+        subject: "Chemistry",
         desctiption: "Code has to be executed after the state has been updated Place that code in the call back function which is the second argument to the setState method.",
     },
     {
-        id:"5",
+        id: "5",
         url: "/course_img.jpg",
-        subject : "English",
+        subject: "English",
         desctiption: "Code has to be executed after the state has been updated Place that code in the call back function which is the second argument to the setState method.",
     },
     {
-        id:"6",
+        id: "6",
         url: "/course_img.jpg",
-        subject : "Hindi",
+        subject: "Hindi",
         desctiption: "Code has to be executed after the state has been updated Place that code in the call back function which is the second argument to the setState method.",
+    },
+
+]
+
+
+export const Progress = [
+    {
+        id: "0",
+        title: 'assignment',
+        subject: "Science",
+        score : "23%"
+    },
+    {
+        id: "2",
+        title: 'assignment',
+        subject: "Maths",
+        score : "23%"
+    },
+    {
+        id: "3",
+        title: 'assignment',
+        subject: "Bio",
+        score : "23%"
+    },
+    {
+        id: "4",
+        title: 'assignment',
+        subject: "Chemistry",
+        score : "23%"
+    },
+    {
+        id: "5",
+        title: 'assignment',
+        subject: "English",
+        score : "23%"
+    },
+    {
+        id: "6",
+        title: 'assignment',
+        subject: "Hindi",
+        score : "23%"
     },
 
 ]
